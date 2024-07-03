95fb4b321a551d4ac44783e422698194b10e90fa
prince <nguhprince1@gmail.com>
2024-07-03 07:13:13 +0100

    Fixed issues encountered with authentication, expenses and userincome apps

--- userincome/views.py
+++ userincome/views.py
@@ -28,7 +28,12 @@ def index(request):
     paginator = Paginator(income, 5)
     page_number = request.GET.get('page')
     page_obj = Paginator.get_page(paginator, page_number)
-    currency = UserPreference.objects.get(user=request.user).currency
+
+    try:
+        currency = UserPreference.objects.get(user=request.user).currency
+    except UserPreference.DoesNotExist as e:
+        currency = None
+        
     context = {
         'income': income,
         'page_obj': page_obj,
